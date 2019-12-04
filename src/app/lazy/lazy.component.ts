04f152138e2a210727528258c2525c06834c0c34
boltex <fmalboeuf@oceanex.com>
2019-12-04 10:57:48 -0500

    ok with init and styles

--- src/app/lazy/lazy.component.ts
+++ src/app/lazy/lazy.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 
 @Component({
   selector: "app-lazy",
   templateUrl: "./lazy.component.html",
   styleUrls: ["./lazy.component.scss"]
 })
-export class LazyComponent implements OnInit {
+export class LazyComponent implements OnInit, AfterViewInit {
   constructor() {
     console.log("component constructor running!");
   }
 
-  ngOnInit() {}
+  public ngOnInit(): void {
+    console.log("on init");
+  }
+
+  public ngAfterViewInit(): void {
+    console.log("after init");
+  }
 }
